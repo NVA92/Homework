fc23d26b08753af78e32860906742b673828894e
NVA92 <nazarov.victor92@yandex.ru>
2017-04-15 11:57:25 +0300

    Homework_№1

---
+++ about.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Урок №1</title>
+</head>
+<body>
+<h1>О себе</h1>
+</body>
+</html>
+
+<?php
+$name ='Victor';
+$age = '24';
+$email = '<a href="Nazarov.victor92@yandex.ru"> Моя почта </a>';
+$about = 'Инженер' ;
+echo 'Имя:',' ',$name;
+echo '<br/>';
+echo 'Возраст:'   , ' ',  $age;
+echo '<br/>';
+echo  'Адрес электронноый почты:', ' ', $email ;
+echo '<br/>';
+echo 'О себе:  ', ' ' ,$about;
+?>
