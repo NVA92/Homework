5d6a8b1874e609e9adb3fd96044a969d4d00a42e
NVA92 <nazarov.victor92@yandex.ru>
2017-04-17 01:21:58 +0300

    Homework2

---
+++ Algorithm.php
@@ -0,0 +1,31 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>header</title>
+</head>
+</html>
+
+<?php
+$u = rand(0, 100);
+echo $u, ' ' . '<br/>' ;
+
+$one = 1;
+$two = 1;
+$three = 0;
+
+while (true) {
+    if ($one > $u) {
+        echo "Задуманное число не входит в числовой ряд";
+        exit;
+    } else if ($one === $u) {
+        echo "Задуманное число входит в числовой ряд";
+        exit;
+    } else {
+        $three = $one;
+        $one = $one + $two;
+        $two = $three;
+    }
+}
+
+?>
